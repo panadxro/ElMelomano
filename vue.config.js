1530a26f78129a00c5028eaa5bccc5ae939d2e1d
Lucas Panadero <lucasn.panadero@gmail.com>
2024-05-05 22:13:13 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/melomano/' // Reemplaza 'nombre-del-subdirectorio' con el nombre real de tu subdirectorio
-    : '/'
-}
+export const publicPath = process.env.NODE_ENV === 'production'
+  ? '/melomano/'
+  : '/';
